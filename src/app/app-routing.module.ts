dc3bd20305306074d0206a29529d455afe541e14
Sachin Dev Tomar <tomarsachindev@gmail.com>
2021-02-19 17:18:30 +0530

    Added routing for the different navigations

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EAndEComponent } from './components/e-and-e/e-and-e.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { SkillsComponent } from './components/skills/skills.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component: HomeComponent},
+  {path:'e-and-e', component: EAndEComponent},
+  {path:'projects', component: ProjectsComponent},
+  {path:'skills', component: SkillsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
