95b61fb9847f77ef650c1c9182756b7a412cfcfa
Sachin Dev Tomar <tomarsachindev@gmail.com>
2021-02-22 12:43:17 +0530

    Updated data for work experience

--- src/app/components/e-and-e/e-and-e.component.ts
+++ src/app/components/e-and-e/e-and-e.component.ts
@@ -7,37 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EAndEComponent implements OnInit {
 
-  loadingText: String = "Loading";
-  timerId: any;
-
   constructor() { }
 
   ngOnInit(): void {
-    this.loadingTextAnimation();
+    
   }
   
   ngOnDestroy(): void{
-    clearInterval(this.timerId);
-  }
-
-  loadingTextAnimation():void{
-    var dotCount = 0;
-    this.timerId = setInterval(() => {
-      dotCount++;
-      switch(dotCount){
-        case 0: this.loadingText = "Loading";
-                break;
-        case 1: this.loadingText = "Loading.";
-                break;
-        case 2: this.loadingText = "Loading..";
-                break;
-        case 3: this.loadingText = "Loading...";
-                break;
-        case 4: this.loadingText = "Loading....";
-                dotCount = 0;
-                break;
-      }
-    }, 300);
   }
 
 }
