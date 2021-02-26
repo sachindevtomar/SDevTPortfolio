60143f87097b8badba9856650007a691ae0618ad
Sachin Dev Tomar <tomarsachindev@gmail.com>
2021-02-27 00:57:23 +0530

    added muliple position in experience component

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -13,4 +13,27 @@ export class ExperienceComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  getDateDifference(startDateString: String, endDateString: String): any{
+    let startDate = new Date(startDateString.toString());
+    let endDate = endDateString.toString() !== "" ? new Date(endDateString.toString()) : new Date();
+    var months;
+    months = (endDate.getFullYear() - startDate.getFullYear()) * 12;
+    months -= startDate.getMonth();
+    months += endDate.getMonth();
+    
+    var dateDiff = "";
+    
+    if(months > 0 && months/12 >= 1){
+      var fyears = parseInt((months/12).toString());
+      var fmonths = months%12;
+      dateDiff = fyears + " years " + (fmonths > 0 ? fmonths + " months" : "");
+    }
+    else if(months > 0){
+        dateDiff = months == 1 ? months + " month": months + " months";
+    }
+    else{
+      dateDiff = "unknown"
+    }
+    return dateDiff;
+  }
 }
